6bee87bd8191a6062983b4f0997a0c75b5493ffe
Martin Cejp <cejp.mart@gmail.com>
2019-08-11 17:04:41 +0200

    Make sure we work offline

--- TSIM.WebServer/wwwroot/js/site.js
+++ TSIM.WebServer/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// code stolen from wherever
+
+function refreshCameras() {
+  $('img.auto-refresh').attr('src', function(i, old) { return old.replace(/\?.+/,"?i=" + (Math.random()*1000)); });
+  setTimeout(refreshCameras, 1000);
+}
+function refreshCamerasFirst() {
+  $('img.auto-refresh').attr('src', function(i, old) { return old + "?i=" + (Math.random()*1000); });
+  setTimeout(refreshCameras, 1000);
+}
+$(function() {
+    setTimeout(refreshCamerasFirst, 1000);
+
+    $('#unit-0-speed-set').click(() => {
+        const data = {'unit': 0, 'speed': Number.parseFloat($('#unit-0-speed').val())};
+        $.ajax({
+        type: "POST",
+        url:'@Url.Action("UnitSpeedSet", "Home")',
+        data: JSON.stringify(data),
+        contentType: 'application/json'
+        });
+    });
+});
