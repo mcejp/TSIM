0bb10d875d399d768f3376cd90e03b0029ac5dd5
Martin Cejp <cejp.mart@gmail.com>
2019-08-11 21:07:53 +0200

    Avoid randomness to achieve repeatable results

--- TSIM/ScenarioLoader.cs
+++ TSIM/ScenarioLoader.cs
@@ -11,17 +11,22 @@ namespace TSIM
 {
     internal class ScenarioDescriptor
     {
-        // Eugh
+        // TODO: mark Required Fields once supported by the language
+
+        // Eugh. Do this better once the language lets us.
 //        [JsonPropertyName("coordinateSystemOrigin")] public (double lat, double lon) CoordinateSystemOrigin { get; set; }
-        [JsonPropertyName("coordinateSystemOrigin")] public double[] CoordinateSystemOrigin { get; set; }
+        public double[] CoordinateSystemOrigin { get; set; }
 
         [JsonPropertyName("networkDatabase")] public string NetworkDatabaseFileName { get; set; }
         [JsonPropertyName("unitClassDatabase")] public string UnitClassDatabaseFileName { get; set; }
-        [JsonPropertyName("units")] public List<UnitDescriptor> Units { get; set; }
+        public List<UnitDescriptor> Units { get; set; }
 
         public class UnitDescriptor
         {
-            [JsonPropertyName("class")] public string Class { get; set; }
+            public string Class { get; set; }
+
+            public float[] Pos { get; set; }
+            public float[] Orientation { get; set; }
         }
     }
 
@@ -47,15 +52,38 @@ namespace TSIM
 
                 foreach (var unitDesc in desc.Units)
                 {
-                    var rand = new Random();
-
-                    // FIXME: randomly pick a segment
-                    var seg = networkDatabase.GetSegmentById(rand.Next(1, 600));
-
                     var class_ = unitClassDatabase.UnitClassByName(unitDesc.Class);
-                    var (pos, dir) = seg.GetPointAndTangent(0.5f, SegmentEndpoint.Start);
-                    var orientation = Utility.DirectionVectorToQuaternion(dir);
-                    units.Add(new Unit(class_, pos, Vector3.Zero, orientation));
+
+                    var randomly = false;        // Place units randomly
+
+                    if (randomly)
+                    {
+                        var rand = new Random();
+
+                        // randomly pick a track segment
+                        var seg = networkDatabase.GetSegmentById(rand.Next(1, 600));
+
+                        var (pos, dir) = seg.GetPointAndTangent(0.5f, SegmentEndpoint.Start);
+                        var orientation = Utility.DirectionVectorToQuaternion(dir);
+
+                        Console.Out.WriteLine(
+                            $"{{\"class\": \"generic\", \"pos\": [{pos.X,7:F1}, {pos.Y,7:F1}, {pos.Z,5:F1}], " +
+                            $"\"orientation\": [{orientation.X,6:F3}, {orientation.Y,6:F3}, {orientation.Z,6:F3}, {orientation.W,5:F3}]}},");
+                        units.Add(new Unit(class_, pos, Vector3.Zero, orientation));
+                    }
+                    else
+                    {
+                        var (x, y, z) = (unitDesc.Pos[0],
+                                         unitDesc.Pos[1],
+                                         unitDesc.Pos[2]);
+                        var pos = new Vector3(x, y, z);
+                        var (xx, yy, zz, ww) = (unitDesc.Orientation[0],
+                                                unitDesc.Orientation[1],
+                                                unitDesc.Orientation[2],
+                                                unitDesc.Orientation[3]);
+                        var orientation = new Quaternion(xx, yy, zz, ww);
+                        units.Add(new Unit(class_, pos, Vector3.Zero, orientation));
+                    }
                 }
 
                 return (coordinateSpace, networkDatabase, unitClassDatabase, units);
@@ -64,7 +92,10 @@ namespace TSIM
 
         private static ScenarioDescriptor LoadScenarioDescriptor(StreamReader file)
         {
-            return JsonSerializer.Deserialize<ScenarioDescriptor>(file.ReadToEnd());
+            var options = new JsonSerializerOptions();
+            options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+
+            return JsonSerializer.Deserialize<ScenarioDescriptor>(file.ReadToEnd(), options);
         }
     }
 }
